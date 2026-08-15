6077fb4886a4fac43c32c399c0bb15c209dd6bc6
b-sushi <yuchiwang27@gmail.com>
2026-08-15 13:25:34 +0100

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        left = 0
+        k = len(s1)
+        right = k
+        characters = {}
+        temp = {}
+        for letter in s1:
+            if letter in characters:
+                characters[letter] +=1
+            else:
+                characters[letter] = 1
+
+        print(characters)
+
+        while right <= len(s2):
+            temp = {}
+            for letter in s2[left:right]:
+                if letter in temp:
+                    temp[letter] += 1
+                else:
+                    temp[letter] = 1
+            print(temp)
+            if temp == characters:
+                return True
+            else:
+                left +=1
+                right +=1
+        return False                
+                    
+
